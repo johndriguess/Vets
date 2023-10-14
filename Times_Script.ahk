fd584c0811b81795fc386877802e29fde566ac48
Johnatan Rodrigues <johndriguess@gmail.com>
2023-10-14 00:44:10 -0300

    Initial commit

---
+++ Times_Script.ahk
@@ -0,0 +1,15 @@
+
+#SingleInstance Force
+#Persistent
+
+OnClipboardChange("historico")
+return
+
+historico()
+{
+	if InStr(Clipboard, "https://cdn.discordapp.com/attachments/1099407064754827374/1100886972106539038/h.png")
+    {
+        Send, cádiz{enter}
+    return
+    }
+}
---
+++ src/main/java/com/vets/vets/Vets.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vets.vets;
+
+/**
+ *
+ * @author John
+ */
+public class Vets {
+
+    public static void main(String[] args) {
+        new Menu().setVisible(true);
+    }
+}
